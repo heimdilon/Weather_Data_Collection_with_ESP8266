214c961f16856fa5cf70a3987aff18cb0efc4955
Orkun Koray GÜNGÖR <73837799+heimdilon@users.noreply.github.com>
2022-01-13 14:38:58 +0300

    Update boot.py

--- boot.py
+++ boot.py
@@ -43,16 +43,12 @@ print(station.ifconfig())
 
 
 def deep_sleep(msecs) :
-  # configure RTC.ALARM0 to be able to wake the device
   rtc = machine.RTC()
   rtc.irq(trigger=rtc.ALARM0, wake=machine.DEEPSLEEP)
 
-  # set RTC.ALARM0 to fire after X milliseconds (waking the device)
   rtc.alarm(rtc.ALARM0, msecs)
 
-  # put the device to sleep
   machine.deepsleep()
-
 
 
 try:
